e40c5ab52fed053b15c9277193d167de0df89def
NanSun1963 <33167636+NanSunPKU@users.noreply.github.com>
2018-01-20 22:52:34 -0600

    IMS EF created.

---
+++ IMS/IMS/IMS/DAL/IMSContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace IMS.DAL
+{
+    public class IMSContext : DbContext
+    {
+        public IMSContext() : base("IMSContext")
+        {
+        }
+        public DbSet<Models.Category> Categories { get; set; }
+        public DbSet<Models.Currency> Currencies { get; set; }
+        public DbSet<Models.Inventory> Inventories { get; set; }
+        public DbSet<Models.ItemPrice> ItemPrices { get; set; }
+        public DbSet<Models.Location> Locations { get; set; }
+        public DbSet<Models.Price> Prices { get; set; }
+        public DbSet<Models.Type> Types { get; set; }
+        public DbSet<Models.Item> Items { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+       
+    }
+}
---
+++ IMS/IMS/IMS/Models/Price.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS.Models
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool TaxInclusive { get; set; }
+        public int CurrencyId { get; set; }
+
+        public virtual Currency Currency { get; set; }
+    }
+}
